4f333590502a1a5deba89733ed294757010b5465
Marco <marco.passini@gmail.com>
2025-11-04 20:26:24 -0500

    Changed Directories

--- HelloGit.java
+++ HelloGit.java
@@ -4,5 +4,6 @@ public static void main (String[] args) {
 	
 	System.out.println("Hello GitHub");
 	System.out.println("Ok kids...ugghhh..you read for this?");
+	System.out.println("Changed Directories");
 }
 }
